0bb0fb9d2f3e47edee2832d5111afd38408c1e06
Ryan <llryanll@yandex.ru>
2023-02-08 00:17:43 +0300

    add 2nd task

---
+++ Task2_AllSimpleNumbers.java
@@ -0,0 +1,17 @@
+// Вывести все простые числа от 1 до 1000
+public class Task2_AllSimpleNumbers {
+    public static void main(String[] args) {
+        for (int i = 2; i < 1001; i++) {
+            Boolean isPrime = true;
+
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime)
+                System.out.println(i);
+        }
+    }
+}
